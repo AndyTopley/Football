983935e6a7a9c41b02605da8107000439774b6ba
Andy Topley <andy@andytopley.co.uk>
2025-03-29 13:42:25 +0000

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Andy Topley on 29/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
